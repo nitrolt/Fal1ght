e9011d68ce6dc9211a57c8dbaef3a430505faeac
Nitrolt <nitrolt145@gmail.com>
2023-06-09 16:09:53 +0700

    nothing to say

--- fal1ght/Assets/Scripts/Movement.cs
+++ fal1ght/Assets/Scripts/Movement.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Collision coll;
+    private Rigidbody2D rb;
+
+    [Header("Stats")]
+    public float speed;
+    public float jumpForce;
+    public float slideSpeed;
+    public float wallJumpLerp;
+    public float dashSpeed;
+
+    [Header("Boolean")]
+    public bool canMove;
+    public bool wallGrab;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool isDashing;
+
+    [Space]
+
+    private bool isGrounded;
+    private bool hasDashed;
+
+    public int side = 1;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+
+    void Flip(int side)
+    {
+        Vector3 Scaler = transform.localScale;
+        Scaler.x = side;
+        transform.localScale = Scaler;
     }
 }
